a7b3273b3e5fe159c2a5518e10f048eee67e5878
Pavlina Antipova <pavlina.antipova@gmail.com>
2021-11-22 20:35:39 +0200

    исправляет 3 задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,52 +3,65 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector(".feedback-form");
 const inputEmailRef = document.querySelector("[type=email]");
 const textAreaMessageRef = document.querySelector("textarea");
-const getLocalStorage = localStorage.getItem("feedback-form-state");
+const STORAGE_KEY = "feedback-form-state";
+const getLocalStorage = localStorage.getItem(STORAGE_KEY);
 const formData = {};
+
+
+inputEmailRef.value = formData.email; 
+textAreaMessageRef.value = formData.message;
+
+
 
 form.addEventListener("input", throttle(onInput, 500));
 form.addEventListener("submit", onSubmit);
 
 autoComplete();
 
+
 function onInput(e) {
     const inputName = e.target.name;
     const inputValue = e.target.value;
     updateStorage(inputName, inputValue);
+    
 }
 
 function onSubmit(e) {
     e.preventDefault();
     console.log(formData);
     form.reset();
-    localStorage.removeItem("feedback-form-state");
-    inputEmailRef.value = "";
-    textAreaMessageRef.value = "";
+    localStorage.removeItem(STORAGE_KEY);
 }
 
 function updateStorage(name, value) {
-    if (value) {
-        formData[name] = value;
-        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-        
+    formData[name] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+    
     }
-}
+
 
 function autoComplete() {
     if (!getLocalStorage) {
-        return;
+        inputEmailRef.value = ""; 
+        textAreaMessageRef.value = "";
     }
 
-    const {email, message} = JSON.parse(getLocalStorage);
-    
+    const { email, message } = JSON.parse(getLocalStorage);
+
     if (email) {
-       inputEmailRef.value = email; 
-    } 
+        inputEmailRef.value = email;
+    } else {
+         inputEmailRef.value = ""; 
+    }
 
     if (message) {
-       textAreaMessageRef.value = message;
+        textAreaMessageRef.value = message;
+    } else {
+         textAreaMessageRef.value = ""; 
     }
-
+    
+    console.log(getLocalStorage);
 }
 
 
